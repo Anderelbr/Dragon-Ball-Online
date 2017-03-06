f36007f0f072500d87364704ba50ffc40a2a8477
Anderelbr <anderelbr20@gmail.com>
2017-03-05 23:24:59 -0300

    Add files via upload

---
+++ NtlNetwork/NtlSharedCommon.h
@@ -0,0 +1,10 @@
+#pragma once
+// WARNING :
+// This header file is assumed to be used only for Win32 or Win64.
+// It has no responsibility for any problems which occurs on another platforms.
+// - YOSHIKI
+
+#include <winsock2.h>
+#include <stdint.h>
+
+#pragma warning(disable:4819) // vs2005 codepage bug disable
